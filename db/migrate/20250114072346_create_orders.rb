7f7724e2366ec69cbc16055e7fd5be4122cfcc95
YukiOzawa1527 <oz.eternal0127@gmail.com>
2025-01-14 09:13:06 +0000

    enum2

--- db/migrate/20250114072346_create_orders.rb
+++ db/migrate/20250114072346_create_orders.rb
@@ -14,7 +14,7 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.integer :status, null: false, default: "0"
 
       enum payment_method: { credit_card: 0, transfer: 1 }
-      enum status: { payment_pending: 0, payment_confirmation: 1, in_production: 2, in_preparation_for_shipping: 3, shipped: 4 }
+      enum status: { payment_pending: 0, payment_confirmation: 1, in_production: 2, in_preparation_for_shipping: 3, shipped: 4 }aa
 
       
     end
