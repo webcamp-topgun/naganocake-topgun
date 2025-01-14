0bd3a97f6346d375182e61a44c85155d3bc93c7a
YukiOzawa1527 <oz.eternal0127@gmail.com>
2025-01-14 08:27:01 +0000

    ozawa1

--- db/migrate/20250114072357_create_order_details.rb
+++ db/migrate/20250114072357_create_order_details.rb
@@ -5,7 +5,7 @@ class CreateOrderDetails < ActiveRecord::Migration[6.1]
       t.integer :order_id, null: false, default: ""
       t.integer :amount, null: false, default: ""
       t.integer :cooking_status, null: false, default: ""
-      t.integer :price, null: false, default: ""   
+      t.integer :price, null: false, default: ""   aa
       t.timestamps
     end
   end
