8e63a04a0cc859e75cf1a6301a53af8cc36ce613
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-14 08:15:02 +0000

    テーブルitems,orders

--- db/migrate/20250114072346_create_orders.rb
+++ db/migrate/20250114072346_create_orders.rb
@@ -12,6 +12,7 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.integer :payment_method, null: false
       t.integer :total_payment, null: false
       t.integer :status, null: false, default: "0"
+      
     end
   end
 end
