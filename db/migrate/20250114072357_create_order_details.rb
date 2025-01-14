1e5882b688c93cd1c35763a76560af6925349535
YukiOzawa1527 <oz.eternal0127@gmail.com>
2025-01-14 08:37:32 +0000

    ozawa５

--- db/migrate/20250114072357_create_order_details.rb
+++ db/migrate/20250114072357_create_order_details.rb
@@ -1,8 +1,6 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
-      t.integer :item_id, null: false, default: ""
-      t.integer :order_id, null: false, default: ""
       t.integer :amount, null: false, default: ""
       t.integer :cooking_status, null: false, default: ""
       t.integer :price, null: false, default: ""
