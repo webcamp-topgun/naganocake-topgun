c28035af974819fdc0f1066d611b80e054ee06ea
YukiOzawa1527 <oz.eternal0127@gmail.com>
2025-01-14 08:34:37 +0000

    ozawa４

--- db/migrate/20250114072228_create_addresses.rb
+++ db/migrate/20250114072228_create_addresses.rb
@@ -1,7 +1,6 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
-      t.integer :customer_id, null: false, default: ""
       t.string :postal_code, null: false, default: ""
       t.string :address, null: false, default: ""
       t.string :name, null: false, default: ""
