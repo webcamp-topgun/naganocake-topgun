d549360c44f63e591ca9871216d850e04fad5923
LovarinJP <187247562+LovarinJP@users.noreply.github.com>
2025-01-14 08:59:03 +0000

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
-  has_many :cart_items, dependent: :destroy
+  has_many :cart_items
+  has_many :customers, through: :cart_items
+
   has_many :order_details, dependent: :destroy
   belongs_to :genre
 end
