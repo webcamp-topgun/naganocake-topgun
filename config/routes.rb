5353310677ed10ae6edf9ff11de6d0ad87341998
YukiOzawa1527 <oz.eternal0127@gmail.com>
2025-01-15 05:07:48 +0000

    admin/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,33 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'order_details/update'
+  end
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :customers
   devise_for :admins
 
