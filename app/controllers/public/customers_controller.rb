4ac9f67ee899e62dc049407ef376bfe10ad6c27f
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-17 04:16:56 +0000

    add

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -6,9 +6,19 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
+    @addresses = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+    flash[:success] = "会員情報が更新されました。"
+    redirect_to my_page_path
+  else
+    render :edit
+  end
+
   end
 
   def unsubscribe
@@ -16,4 +26,6 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
+
+
 end
