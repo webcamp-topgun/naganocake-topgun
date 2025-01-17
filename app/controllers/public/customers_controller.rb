7d5e1ee24dc7fdf44929d495cb515ed27d897159
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-17 06:19:21 +0000

    顧客編集画面の更新機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -27,5 +27,10 @@ class Public::CustomersController < ApplicationController
   def withdraw
   end
 
+  private
 
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
+  end
+  
 end
