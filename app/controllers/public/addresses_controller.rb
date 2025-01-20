e122a7f631d64af468c6f98145af24fd7fc195b8
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-20 07:29:43 +0000

    配達先登録・一覧

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,16 +2,19 @@ class Public::AddressesController < ApplicationController
 
   def index
     @address = Address.new
-    @addresses = Address.all
+    @customer = current_customer
+    @addresses = @customer.addresses
+
   end
 
   def edit
   end
 
   def create
-    address = Address.new(address.params)
-    address.save
-    redirect_to '/addresses'
+    @address = Address.new(address_params)
+    @address.customer_id =current_customer.id
+    @address.save
+    redirect_to addresses_path
   end
 
   def update
--- db/schema.rb
+++ db/schema.rb
@@ -41,16 +41,17 @@ ActiveRecord::Schema.define(version: 2025_01_14_072357) do
   end
 
   create_table "addresses", force: :cascade do |t|
-    t.string "postal_code", default: "", null: false
-    t.string "address", default: "", null: false
-    t.string "name", default: "", null: false
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -64,19 +65,21 @@ ActiveRecord::Schema.define(version: 2025_01_14_072357) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "amount", null: false
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string "last_name", default: "", null: false
-    t.string "first_name", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
     t.string "last_name_kana", null: false
-    t.string "first_name_kana", default: "", null: false
-    t.string "postal_code", default: "", null: false
-    t.string "address", default: "", null: false
-    t.string "phone_number", default: "", null: false
+    t.string "first_name_kana", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
     t.boolean "is_active", default: true, null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -87,7 +90,7 @@ ActiveRecord::Schema.define(version: 2025_01_14_072357) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name", default: "", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -103,8 +106,10 @@ ActiveRecord::Schema.define(version: 2025_01_14_072357) do
   end
 
   create_table "order_details", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "order_id", null: false
     t.integer "amount", null: false
-    t.integer "cooking_status", null: false
+    t.integer "cooking_status", default: 0, null: false
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -113,6 +118,7 @@ ActiveRecord::Schema.define(version: 2025_01_14_072357) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id", null: false
     t.string "name", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
