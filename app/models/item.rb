df8c4cd37b4ef967a12a0cc3ccd81dc3534da161
LovarinJP <187247562+LovarinJP@users.noreply.github.com>
2025-01-20 09:24:23 +0000

    fix-items

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,6 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :genre
 
-  validates :image, presence: true
-
   def with_tax_price
     (price * 1.1).floor
   end
