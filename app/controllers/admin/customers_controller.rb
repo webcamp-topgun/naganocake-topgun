b12840f0e88e56bb1dd128d92d79474d80f30680
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-20 05:26:27 +0000

    修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,5 +14,14 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
+
+  private
+def customer_params
+  params.require(:customer).permit(:is_active)
+end
+
 end
