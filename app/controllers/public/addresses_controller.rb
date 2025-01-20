352c0100c456b087228b647ccea56816030efd4c
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-20 09:23:08 +0000

    配達先一覧の削除機能追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -21,6 +21,9 @@ class Public::AddressesController < ApplicationController
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
 
   def address_params
