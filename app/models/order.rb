095ea6be600280e9c671e7665ba4503021e0eca3
YukiOzawa1527 <oz.eternal0127@gmail.com>
2025-01-20 07:29:39 +0000

    fix120

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,10 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { payment_pending: 0, payment_confirmation: 1, in_production: 2, in_preparation_for_shipping: 3, shipped: 4 }
 
+  def subtotal
+    item.with_tax_price * amount
+  end
+
   def status_jp
     I18n.t("enums.order.status.#{status}")
   end
