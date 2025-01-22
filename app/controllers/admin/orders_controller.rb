5cdb3a370aeb65424230d5c7b7ac4a19632e59ad
LovarinJP <187247562+LovarinJP@users.noreply.github.com>
2025-01-22 09:51:17 +0000

    fix-admin-order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,20 +3,19 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
-    @order.postage = 800
-    @total_payment = @order_details.sum(:subtotal)
-    @order.total_payment = @order.postage + @total
+    @sum = 0
+    @total_payment = 0
   end
 
   def update
     @order = Order.find(params[:id])
-    @order.status.update(order_params)
+    @order.update(order_params)
     redirect_to admin_order_path(@order.id)
   end
 
   private
 
   def order_params
-    params.require(:@order).permit(:status)
+    params.require(:order).permit(:status)
   end
 end
