bc0d3f6fd67b191fb4074507183f65fb60233fa0
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-24 08:00:35 +0000

    fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,10 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { payment_pending: 0, payment_confirmation: 1, in_production: 2, in_preparation_for_shipping: 3, shipped: 4 }
 
+  validates :address, presence: true
+  validates :postal_code, presence: true
+  validates :name, presence: true
+
   def subtotal
     item.with_tax_price * amount
   end
