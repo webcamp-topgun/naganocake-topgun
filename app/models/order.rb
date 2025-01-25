8b31f35e77fca610d30a95c4274c4264dda213fb
YukiOzawa1527 <oz.eternal0127@gmail.com>
2025-01-25 02:39:39 +0000

    125

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,12 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { payment_pending: 0, payment_confirmation: 1, in_production: 2, in_preparation_for_shipping: 3, shipped: 4 }
 
+  validates :payment_method, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :postal_code, presence: true
+ 
+
   def subtotal
     item.with_tax_price * amount
   end
